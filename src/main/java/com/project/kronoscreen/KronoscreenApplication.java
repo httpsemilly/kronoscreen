7daaf1b8525dd749548546c160b91c9e94318585
Emilly Cavalcante <emilly.menezescs@gmail.com>
2026-01-08 15:49:20 -0300

    chore: initial project setup with Spring Initializr

---
+++ src/main/java/com/project/kronoscreen/KronoscreenApplication.java
@@ -0,0 +1,13 @@
+package com.project.kronoscreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KronoscreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KronoscreenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/kronoscreen/KronoscreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.kronoscreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KronoscreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
